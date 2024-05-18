ab045329744cb48e3d2039818fb03f009fa7f784
TridentTD <prote2000@gmail.com>
2024-05-19 05:37:47 +0700

    version 1.0.2
    fix WiFiClientSecure bug ไม่สามารถ รองรับ response ยาวๆได้
    ใช้ http.getString() แทน

--- src/BlynkGO_ThaiCal.hpp
+++ src/BlynkGO_ThaiCal.hpp
@@ -6,7 +6,7 @@
 /** Minor version number (x.X.x) */
 #define THAICAL_VERSION_MINOR   0
 /** Patch version number (x.x.X) */
-#define THAICAL_VERSION_PATCH   1
+#define THAICAL_VERSION_PATCH   2
 
 #define THAICAL_VERSION_TEXT    (String(THAICAL_VERSION_MAJOR)+"."+String(THAICAL_VERSION_MINOR)+"."+String(THAICAL_VERSION_PATCH))
 
@@ -42,105 +42,141 @@
   #include <HTTPClient.h>
 #endif
 
-
 /* พ.ศ.2540 - ปีปัจจุบัน */
-class BlynkGO_ThaiCal {
+class BlynkGO_ThaiCal
+{
   public:
     BlynkGO_ThaiCal() {};
 
-    static String get(uint8_t day /* 1-31*/, uint8_t month /* 1-12 */, uint16_t year /* ei. 2024 */,  bool *is_wanpra=NULL, String *wan_text=NULL, String *year_naksatra=NULL) {
-      if(is_wanpra !=NULL)      { *is_wanpra      = false;  }
-      if(wan_text  !=NULL)      { *wan_text       = "";     }
-      if(year_naksatra  !=NULL) { *year_naksatra  = "";     }
+    static String get(uint8_t day /* 1-31*/, uint8_t month /* 1-12 */, uint16_t year /* ei. 2024 */,  bool * is_wanpra = NULL, String * wan_text = NULL, String * year_naksatra = NULL)
+    {
+      if(is_wanpra != NULL)      {
+        *is_wanpra      = false;
+      }
+      if(wan_text  != NULL)      {
+        *wan_text       = "";
+      }
+      if(year_naksatra  != NULL) {
+        *year_naksatra  = "";
+      }
 
       String _thai_date = "";
-      int16_t _th_year  = year+543;
-      if( _th_year < 2540 ) return _thai_date;
+      int16_t _th_year  = year + 543;
+      if(_th_year < 2540) return _thai_date;
 
-      char   _cur_date[10]; snprintf(_cur_date, sizeof(_cur_date), "%04d%02d%02d", year, month, day);
+      char   _cur_date[10];
+      snprintf(_cur_date, sizeof(_cur_date), "%04d%02d%02d", year, month, day);
       // Serial.printf( "_cur_date : %s\n", _cur_date);
 
       HTTPClient http;
       WiFiClientSecure client_ssl;
       client_ssl.setInsecure();
 
+      // https://www.myhora.com/calendar/ical/thai.aspx?2567.ical
       http.begin(client_ssl, "https://www.myhora.com/calendar/ical/thai.aspx?" + String(_th_year) + ".ical");
-      if(http.GET() != 0){
-        if(client_ssl.available()){
-          while(client_ssl.available()){
-            String line_data = client_ssl.readStringUntil('\n'); line_data.trim();
+      int httpcode = http.GET();
+      // Serial.println(httpcode);
+      if(httpcode == 200 || httpcode == -1) {
+        String data = http.getString();
+        // -- sample บรรทัดที่สำคัญ --
+        // DTSTART;VALUE=DATE:20240118
+        // DESCRIPTION:วันพฤหัสบดี ขึ้น 8 ค่ำ เดือนยี่(๒) ปีเถาะ
+        // SUMMARY:ขึ้น 14 ค่ำ เดือนแปด(๘)
+        // SUMMARY:ขึ้น 8 ค่ำ เดือนยี่(๒) วันพระ
+        // SUMMARY:ขึ้น 15 ค่ำ เดือนแปด(๘) วันอาสาฬหบูชา
+        // SUMMARY:แรม 1 ค่ำ เดือนแปด(๘) วันเข้าพรรษา
 
-            // -- sample บรรทัดที่สำคัญ --
-            // DTSTART;VALUE=DATE:20240118
-            // DESCRIPTION:วันพฤหัสบดี ขึ้น 8 ค่ำ เดือนยี่(๒) ปีเถาะ
-            // SUMMARY:ขึ้น 14 ค่ำ เดือนแปด(๘)
-            // SUMMARY:ขึ้น 8 ค่ำ เดือนยี่(๒) วันพระ
-            // SUMMARY:ขึ้น 15 ค่ำ เดือนแปด(๘) วันอาสาฬหบูชา
-            // SUMMARY:แรม 1 ค่ำ เดือนแปด(๘) วันเข้าพรรษา
+        // ------------------------------------- [ VEVENT ]
+        // BEGIN:VEVENT
+        // DTSTART;VALUE=DATE:20241229
+        // DTEND;VALUE=DATE:20241229
+        // DTSTAMP:20240519T043306Z
+        // UID:20241229-0WMU
+        // CLASS:PUBLIC
+        // CREATED:20100101T000000Z
+        // DESCRIPTION:วันอาทิตย์ แรม 14 ค่ำ เดือนอ้าย(๑) ปีมะโรง
+        // LAST-MODIFIED:20240519T043306Z
+        // LOCATION:
+        // SEQUENCE:0
+        // STATUS:CONFIRMED
+        // SUMMARY:แรม 14 ค่ำ เดือนอ้าย(๑) วันพระ
+        // TRANSP:OPAQUE
+        // END:VEVENT
+        // -------------------------------------
 
-            if(line_data.startsWith("DTSTART;VALUE=DATE:")){
-              line_data.replace("DTSTART;VALUE=DATE:",""); line_data.trim();
-              // Serial.printf("[DATE] %s\n", line_data.c_str());
-              if( line_data == _cur_date) {
-                while(client_ssl.available()){
-                  String line_data2 = client_ssl.readStringUntil('\n'); line_data2.trim();
-                  if(line_data2.startsWith("SUMMARY")) {
-                    line_data2.replace("SUMMARY:", ""); line_data.trim();
+        String checker = String("DTSTART;VALUE=DATE:") + _cur_date;
+        String line_data;
+        int id = data.indexOf(checker);
+        if(id >= 0) {
+          int id2 = data.indexOf("SUMMARY", id + 1);
+          int id3 = data.indexOf("\n", id2 + 1);
+          line_data = data.substring(id2, id3);
+          line_data.replace("SUMMARY:", "");
+          line_data.trim();
+          if(line_data.indexOf(" วัน") > 0) {
+            _thai_date = line_data.substring(0, line_data.indexOf(" วัน"));
+            String _wan_text = line_data.substring(line_data.indexOf(" วัน") + 1);
+            if(wan_text != NULL) *wan_text = _wan_text;
+            if(is_wanpra != NULL) *is_wanpra = (_wan_text != "" && _wan_text != "วันเข้าพรรษา");
+          }
+          else {
+            _thai_date = line_data;
+          }
 
-                    if(line_data2.indexOf(" วัน") > 0) {
-                      _thai_date = line_data2.substring( 0, line_data2.indexOf(" วัน") );
-                      String _wan_text = line_data2.substring( line_data2.indexOf(" วัน")+1 );
-                      if(wan_text != NULL) *wan_text = _wan_text;
-                      if(is_wanpra !=NULL) *is_wanpra =  ( _wan_text != "" && _wan_text != "วันเข้าพรรษา" );
-                    }else{
-                      _thai_date = line_data2;
-                    }
-                    break;
-                  }
-
-                  if( year_naksatra != NULL ) {
-                    if(line_data2.startsWith("DESCRIPTION")) {
-                      *year_naksatra = line_data2.substring( line_data2.indexOf(" ปี") + 1 );
-                    }
-                  }
-                }
-                break;
-              }
-            }
+          if(year_naksatra != NULL) {
+            int id4 = data.indexOf("DESCRIPTION", id + 1);
+            int id5 = data.indexOf("\n", id4 + 1);
+            line_data = data.substring(id4, id5);
+            line_data.trim();
+            *year_naksatra = line_data.substring(line_data.indexOf(" ปี") + 1);
+            Serial.println(*year_naksatra);
           }
         }
+        else {
+          Serial.println("[ThaiCal] error : not found the date");
+        }
+      }
+      else {
+        Serial.println("[ThaiCal] error : myhora connection error");
       }
       return _thai_date;
     }
 
-    static int8_t is_wanpra(uint8_t day /* 1-31*/, uint8_t month /* 1-12 */, uint16_t year /* ei. 2024 */  ) {
-      int16_t _th_year  = year+543;
-      if( _th_year < 2540 ) return ERROR_LOWER_Y2540;  // error : ปีพ.ศ.ต่ำว่า 2540
+    static int8_t is_wanpra(uint8_t day /* 1-31*/, uint8_t month /* 1-12 */, uint16_t year /* ei. 2024 */)
+    {
+      int16_t _th_year  = year + 543;
+      if(_th_year < 2540) return ERROR_LOWER_Y2540;    // error : ปีพ.ศ.ต่ำว่า 2540
 
-      char   _cur_date[10]; snprintf(_cur_date, sizeof(_cur_date), "%04d%02d%02d", year, month, day);
-      
+      char   _cur_date[10];
+      snprintf(_cur_date, sizeof(_cur_date), "%04d%02d%02d", year, month, day);
+
       HTTPClient http;
       WiFiClientSecure client_ssl;
       client_ssl.setInsecure();
 
+      // https://www.myhora.com/calendar/ical/buddha.aspx?2567.ical
       http.begin(client_ssl, "https://www.myhora.com/calendar/ical/buddha.aspx?" + String(_th_year) + ".ical");
-      if(http.GET() != 0){
-        if(client_ssl.available()){
-          while(client_ssl.available()){
-            String line_data = client_ssl.readStringUntil('\n'); line_data.trim();
-            if(line_data.startsWith("DTSTART;VALUE=DATE:")){
-              line_data.replace("DTSTART;VALUE=DATE:",""); line_data.trim();
+      if(http.GET() != 0) {
+        if(client_ssl.available()) {
+          while(client_ssl.available()) {
+            String line_data = client_ssl.readStringUntil('\n');
+            line_data.trim();
+            if(line_data.startsWith("DTSTART;VALUE=DATE:")) {
+              line_data.replace("DTSTART;VALUE=DATE:", "");
+              line_data.trim();
               // Serial.printf("[DATE] %s\n", line_data.c_str());
-              if( line_data == _cur_date) {
+              if(line_data == _cur_date) {
                 return 1;   // ใช่ วันพระ
               }
             }
           }
           return 0;   // ไม่ใช่วันพระ
-        }else{
+        }
+        else {
           return ERROR_HTTP_FAIL;  // error: HTTP FAIL
         }
-      }else{
+      }
+      else {
         return ERROR_HTTP_FAIL;  // error: HTTP FAIL
       }
     }
